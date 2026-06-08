de6284ead715907c53c4049329225bb0fbd08abc
Jan Zechovský <email@zechy.cz>
2026-06-08 22:31:46 +0200

    docs: Chance extension method inherits from DiceBag.

--- srcs/csharp/Sagittaras.Dices/Extensions/ChanceExtension.cs
+++ srcs/csharp/Sagittaras.Dices/Extensions/ChanceExtension.cs
@@ -4,11 +4,7 @@ namespace Sagittaras.Dices.Extensions
 {
     public static class ChanceExtension
     {
-        /// <summary>
-        ///     Evaluates whether a given chance succeeds, based on the internal probability logic.
-        /// </summary>
-        /// <param name="chance">The chance to be evaluated, represented by a <see cref="Chance"/> instance.</param>
-        /// <returns>Returns true if the chance succeeds; otherwise, false.</returns>
+        /// <inheritdoc cref="DiceBag.TryChance(Chance)" />
         public static bool Try(this Chance chance)
         {
             return DiceBag.TryChance(chance);
