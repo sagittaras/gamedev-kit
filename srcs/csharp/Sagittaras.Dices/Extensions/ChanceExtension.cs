5281a7762c7bee943f70a07eda6e11f6e152c7ba
Jan Zechovský <email@zechy.cz>
2026-06-09 11:46:47 +0200

    feat: Added WeightedPick extension method.
    Serves as a special case of FlipCoin, picking one of two values based on the given chance.

--- srcs/csharp/Sagittaras.Dices/Extensions/ChanceExtension.cs
+++ srcs/csharp/Sagittaras.Dices/Extensions/ChanceExtension.cs
@@ -11,6 +11,20 @@ namespace Sagittaras.Dices.Extensions
         }
 
         /// <summary>
+        ///     Picks one of the two provided elements based on the given chance.
+        /// </summary>
+        /// <param name="chance">The probability to select the first element.</param>
+        /// <param name="a">The first element to be picked.</param>
+        /// <param name="b">The second element to be picked.</param>
+        /// <param name="diceBag">An optional custom dice bag for random number generation. If not provided, the default instance is used.</param>
+        /// <typeparam name="T">The type of elements to pick from.</typeparam>
+        /// <returns>Returns the selected element based on the weighted chance.</returns>
+        public static T WeightedPick<T>(this Chance chance, T a, T b, IDiceBag? diceBag = null)
+        {
+            return (diceBag ?? DiceBag.Instance).WeightedPick(chance, a, b);
+        }
+
+        /// <summary>
         ///     Normalizes the current instance of <see cref="Chance"/> based on the provided probability sum.
         /// </summary>
         /// <param name="chance">The <see cref="Chance"/> instance to normalize.</param>
--- srcs/csharp/Sagittaras.Dices/Extensions/DiceBagExtension.cs
+++ srcs/csharp/Sagittaras.Dices/Extensions/DiceBagExtension.cs
@@ -18,7 +18,23 @@ namespace Sagittaras.Dices.Extensions
         {
             return Chance.Half.Try(diceBag) ? a : b;
         }
-        
+
+        /// <summary>
+        ///     Selects one of two options based on a weighted probability.
+        /// </summary>
+        /// <typeparam name="T">The type of the values to be evaluated and returned.</typeparam>
+        /// <param name="diceBag">The dice bag used to determine the outcome using the given probability.</param>
+        /// <param name="chance">The probability value representing the likelihood of selecting <paramref name="a"/>.</param>
+        /// <param name="a">The value to return if the weighted probability evaluation succeeds.</param>
+        /// <param name="b">The value to return if the weighted probability evaluation fails.</param>
+        /// <returns>
+        ///     Returns <paramref name="a"/> with a probability based on <paramref name="chance"/>, otherwise returns <paramref name="b"/>.
+        /// </returns>
+        public static T WeightedPick<T>(this IDiceBag diceBag, Chance chance, T a, T b)
+        {
+            return diceBag.Try(chance) ? a : b;
+        }
+
         /// <summary>
         ///     Generates a new random <see cref="Chance"/> value within the predefined range.
         /// </summary>
