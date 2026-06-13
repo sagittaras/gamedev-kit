588aaadd718396ac1e556e00a42efd066af7cd98
Jan Zechovský <email@zechy.cz>
2026-06-13 14:23:17 +0200

    feat: Introduced the Timing package.

---
+++ srcs/csharp/Sagittaras.Timing/Cooldown.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Sagittaras.GuardClauses;
+using Sagittaras.GuardClauses.Extensions;
+
+namespace Sagittaras.Timing
+{
+    /// <summary>
+    ///     Timer tracking and managing a cooldown period, allowing controlled execution of time-dependent actions.
+    /// </summary>
+    public class Cooldown
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="Cooldown"/> with the specified duration.
+        /// </summary>
+        /// <param name="duration">The duration of the cooldown in seconds.</param>
+        /// <param name="startsOnCooldown">Indicates whether the cooldown should start as NOT ready.</param>
+        public Cooldown(float duration, bool startsOnCooldown = false)
+        {
+            Guard.Against.LessThanZero(duration);
+            
+            Duration = duration;
+            ElapsedTime = startsOnCooldown ? 0 : duration;
+        }
+
+        /// <summary>
+        ///     Creates a new instance of <see cref="Cooldown"/> configured by <see cref="TimeSpan"/> instance.
+        /// </summary>
+        /// <param name="duration">Instance of <see cref="TimeSpan"/> configuring the duration of the cooldown.</param>
+        /// <param name="startsOnCooldown">Indicates whether the cooldown should start as NOT ready.</param>
+        public Cooldown(TimeSpan duration, bool startsOnCooldown = false) : this((float)duration.TotalSeconds, startsOnCooldown)
+        {
+        }
+        
+        /// <summary>
+        ///     Duration of the cooldown in seconds.
+        /// </summary>
+        public float Duration { get; }
+
+        /// <summary>
+        ///     Currently remaining time of the cooldown in seconds.
+        /// </summary>
+        public float Remaining => Math.Max(0, Duration - ElapsedTime);
+
+        /// <summary>
+        ///     Indicates whether the cooldown is ready to be used.
+        /// </summary>
+        public bool IsReady => Remaining <= 0;
+        
+        /// <summary>
+        ///     Watches the accumulation of time between <see cref="Update"/> calls.
+        /// </summary>
+        private float ElapsedTime { get; set; }
+
+        /// <summary>
+        ///     Updates the accumulation towards the cooldown duration.
+        /// </summary>
+        /// <param name="deltaTime">Delta by which the accumulation of time is increased.</param>
+        public void Update(float deltaTime)
+        {
+            ElapsedTime += deltaTime;
+        }
+        
+        /// <summary>
+        ///     Resets the accumulated time marking the <see cref="Cooldown"/> as not ready.
+        /// </summary>
+        public void Reset()
+        {
+            ElapsedTime = 0;
+        }
+    }
+}
