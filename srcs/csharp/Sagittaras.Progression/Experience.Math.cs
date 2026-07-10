50f9517716fa6ece1735ae6ce3e2ef539394fe20
Jan Zechovský <email@zechy.cz>
2026-07-10 11:05:35 +0200

    test: Test of Level.Gain logic.

--- srcs/csharp/Sagittaras.Progression/Experience.Math.cs
+++ srcs/csharp/Sagittaras.Progression/Experience.Math.cs
@@ -6,10 +6,20 @@ namespace Sagittaras.Progression
         {
             return new Experience(left.Value + right.Value);
         }
+
+        public static Experience operator +(Experience left, int right)
+        {
+            return new Experience(left.Value + right);
+        }
         
         public static Experience operator -(Experience left, Experience right)
         {
             return new Experience(left.Value - right.Value);
+        }
+        
+        public static Experience operator -(Experience left, int right)
+        {
+            return new Experience(left.Value - right);
         }
     }
 }
---
+++ srcs/csharp/Sagittaras.Tests.Progression/LevelTest.cs
@@ -0,0 +1,53 @@
+﻿using AwesomeAssertions;
+using Sagittaras.Progression;
+using Sagittaras.Progression.Formulas;
+
+namespace Sagittaras.Tests.Progression;
+
+public class LevelTest
+{
+    /// <summary>
+    ///     Verify behavior of <see cref="Level.Gain"/> for single level.
+    /// </summary>
+    [Fact]
+    public void Gain_SingleLevel()
+    {
+        const int overflow = 50;
+        
+        ExponentialScalingFormula formula = new();
+        Level level = new(1, formula);
+        
+        LevelGainResult result = level.Gain(formula.Calculate(1) + overflow);
+        result.LevelsGained.Should().Be(1);
+        result.LeveledUp.Should().BeTrue();
+
+        Level current = result;
+        current.Progress.Current.Should().Be(overflow);
+    }
+
+    /// <summary>
+    ///     Verify behavior of <see cref="Level.Gain"/> for multiple levels at once.
+    /// </summary>
+    [Fact]
+    public void Gain_MultipleLevels()
+    {
+        const int levelsToGain = 4;
+        
+        ExponentialScalingFormula formula = new();
+        Level level = new(1, formula);
+        Experience toGain = Experience.Zero;
+
+        for (int i = 0; i < levelsToGain; i++)
+        {
+            toGain += formula.Calculate(i + 1);
+        }
+
+        LevelGainResult result = level.Gain(toGain);
+        result.LevelsGained.Should().Be(levelsToGain);
+        result.LeveledUp.Should().BeTrue();
+
+        Level current = result;
+        current.Value.Should().Be(levelsToGain + 1);
+        current.Progress.Current.Should().Be(Experience.Zero);
+    }
+}
